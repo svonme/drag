6ccbef9591cd037572033f450fffd3b19001bc07
svon <svon.me@gmail.com>
2019-06-25 19:37:25 +0800

    优化

--- js/drag.js
+++ js/drag.js
@@ -5,157 +5,186 @@ define(['$', '_vue'], function($, Vue) {
     // 绑定事件
     function on(element, name, callback) {
         function app(e) {
-            e.preventDefault(); // 取消事件默认行为
-            e.stopPropagation(); // 取消事件冒泡
+            e.preventDefault() // 取消事件默认行为
+            e.stopPropagation() // 取消事件冒泡
             if (callback) {
-                callback(e);
+                callback(e)
             }
         }
-        $(element).on(name, app);
+        $(element).on(name, app)
     }
     // 删除事件
     function off(element, name) {
-        $(element).off(name);
+        $(element).off(name)
     }
     // 添加 class
     function addClass(el, name) {
-        var value = [].concat(name);
-        $(el).addClass(value.join(' '));
+        var value = [].concat(name)
+        $(el).addClass(value.join(' '))
     }
     // 删除 class
     function removeClass(el, name) {
-        var value = [].concat(name);
-        $(el).removeClass(value.join(' '));
+        var value = [].concat(name)
+        $(el).removeClass(value.join(' '))
     }
     // 设置样式
     function style(el, opt) {
-        $(el).css(opt);
+        $(el).css(opt)
     }
     // 获取元素坐标
     function offset (el) {
-        var $dom = $(el);
-        var _offset = $dom.offset();
-        _offset['right'] = _offset.left + $dom.innerWidth();
-        _offset['bottom'] = _offset.top + $dom.innerHeight();
-        return _offset;
+        var $dom = $(el)
+        var _offset = $dom.offset()
+        _offset['right'] = _offset.left + $dom.innerWidth()
+        _offset['bottom'] = _offset.top + $dom.innerHeight()
+        return _offset
     }
     // 计算元素重叠
     function overlap(drag, origin) {
-        var offset1 = offset(drag);
-        var dom = $(origin);
-        var index = -1;
+        let offset1 = offset(drag)
+
+        let $_origins = $(origin)
+        let dom = []
+
+        $.each($_origins, function(i, el){
+            $.each($_origins, function(j, parent){
+                let flag = true
+                if (el !== parent) {
+                    let closest = $(el).closest(parent)
+                    if (closest.length > 0) {
+                        dom = [].concat(el, dom)
+                        flag = false
+                    }
+                }
+                if (flag) {
+                    let status = true
+                    for(let index = 0, size = dom.length; index < size; index++) {
+                        if(dom[index] === el) {
+                            status = false
+                            break
+                        }
+                    }
+                    if (status) {
+                        dom.push(el)
+                    }
+                }
+            })
+        })
+
+        let index = -1
         for(var i = 0, size = dom.length; i < size; i++) {
-            var offset2 = offset(dom.eq(i));
+            let offset2 = offset(dom[i])
             if (offset1.left > offset2.left && offset1.right < offset2.right) {
                 if (offset1.top > offset2.top && offset1.bottom < offset2.bottom) {
-                    index = i;
-                    break;
+                    index = i
+                    break
                 }
             }
         }
         if (index >= 0) {
-            return dom.get(index);
+            return dom[index]
         }
-        return false;
+        return false
     }
 
     function Drag(el, opt, VNode) {
         if (!opt.value) {
-            return false;
+            return false
         }
-        var down = false;
-        var lap = false;
-        var time;
-        var dom;
+        var down = false
+        var lap = false
+        var time
+        var dom
 
         function getDom () {
-            var data = VNode['data'] || {};
-            var attrs = data['attrs'] || {};
+            var data = VNode['data'] || {}
+            var attrs = data['attrs'] || {}
             if (attrs.clone) {
-                var tmp = $(el).clone();
-                $(el).after(tmp);
-                return tmp;
+                var tmp = $(el).clone()
+                $(el).after(tmp)
+                return tmp
             }
-            return $(el);
+            return $(el)
         }
         function removeDom (status) {
-            var data = VNode['data'] || {};
-            var attrs = data['attrs'] || {};
+            var data = VNode['data'] || {}
+            var attrs = data['attrs'] || {}
             if (attrs.clone) {
-                $(dom).remove();
+                $(dom).remove()
             }
             if (status && attrs.remove) {
-                $(el).remove();
+                $(el).remove()
             }
-            dom = false;
+            dom = false
         }
         // 绑定鼠标按下事件
         on(el, 'mousedown.drag', function (e) {
-            down = true; // 按下鼠标
+            down = true // 按下鼠标
             if (!dom) {
-                dom = getDom();
+                dom = getDom()
             }
-            addClass(dom, 'drag-on'); // 标记开始拖动
+            addClass(dom, 'drag-on') // 标记开始拖动
             style(dom, {
                 left: e.clientX + 'px',
                 top: e.clientY + 'px',
-            });
+            })
 
-            time = new Date().getTime();
+            time = new Date().getTime()
             // 鼠标移动
             on(document, 'mousemove.' + time, function (emove) {
                 if (down) {
-                    addClass(dom, 'drag-move'); // 拖动中
+                    addClass(dom, 'drag-move') // 拖动中
                     style(dom, {
                         left: (emove.clientX - $(el).width() / 2) + 'px',
                         top: (emove.clientY - $(el).height() / 2) + 'px',
-                    });
-                    lap = overlap(dom, opt.value);
+                    })
+                    lap = overlap(dom, opt.value)
                     if (lap) {
-                        addClass(lap, 'hover');
+                        removeClass(opt.value, 'hover')
+                        addClass(lap, 'hover')
                     } else {
-                        lap = false;
-                        removeClass(opt.value, 'hover');
+                        lap = false
+                        removeClass(opt.value, 'hover')
                     }
                 }
-            });
+            })
             // 鼠标抬起
             on(dom, 'mouseup.drag', function () {
-                off(document, 'mousemove.' + time); // 删除 move 事件
-                removeClass(dom, ['drag-on', 'drag-move']); // 拖动完成删除拖动标识
+                off(document, 'mousemove.' + time) // 删除 move 事件
+                removeClass(dom, ['drag-on', 'drag-move']) // 拖动完成删除拖动标识
                 style(dom, {
                     left: '0',
                     top: '0',
-                });
+                })
                 if (lap) {
                     if (VNode.data && VNode.data.attrs) {
-                        var append = VNode.data.attrs['append'];
+                        var append = VNode.data.attrs['append']
                         if (append) {
-                            var clone = $(el).get(0);
-                            $(lap).append(clone);
+                            var clone = $(el).get(0)
+                            $(lap).append(clone)
                         }
                     }
                     if (VNode.data && VNode.data.on) {
-                        var dragsuccess = VNode.data.on['dragsuccess'];
+                        var dragsuccess = VNode.data.on['dragsuccess']
                         if (dragsuccess) {
                             dragsuccess({
                                 target: el,
                                 origin: lap
-                            });
+                            })
                         }
                     }
                 }
-                removeDom(lap);
-                lap = false;
-                down = false; // 鼠标弹起
-                removeClass(opt.value, 'hover');
-            });
-        });
+                removeDom(lap)
+                lap = false
+                down = false // 鼠标弹起
+                removeClass(opt.value, 'hover')
+            })
+        })
     }
     // 设置指令
     Vue.directive('drag', {
         inserted: function(el, opt, VNode){
-            Drag(el, opt, VNode);
+            Drag(el, opt, VNode)
         },
         // 准备
         bind: function (el, opt) {
@@ -166,8 +195,8 @@ define(['$', '_vue'], function($, Vue) {
         // 清理, 删除
         unbind: function (el, opt) {
             // 删除事件
-            off(el, 'mousedown.drag');
-            off(el, 'mouseup.drag');
+            off(el, 'mousedown.drag')
+            off(el, 'mouseup.drag')
         }
-    });
+    })
 })
--- js/main.js
+++ js/main.js
@@ -4,26 +4,44 @@ define(['vue', '$'], function(Vue, $) {
         el: '.main',
         data: {
             list: [1,2,3,4,5,6,7,8,9,10],
-            con1: [],
-            con2: [],
+            dragList: []
+        },
+        computed: {
+            dragQuerySelector() {
+                return '.content, .content > .item'
+            }
         },
         methods: {
             includes: function (value) {
-                if (this.con1.includes(value) || this.con2.includes(value)) {
-                    return true;
+                let status = false
+                for(let i = 0; i < this.dragList.length; i++) {
+                    let item = this.dragList[i]
+                    if (item.includes(value)) {
+                        status = true
+                        break
+                    }
                 }
-                return false;
+                return status
             },
-            success: function (id, e) {
-                var origin = $(e['origin']);
-                var key = origin.data('key');
-                var data = [].concat(this[key]);
-                data.push(id);
-                var obj = {};
-                obj[key] = data;
-                Object.assign(this.$data, obj);
+            success: function (name, e) {
+                let dragList = [].concat(this.dragList)
+                let origin = $(e['origin'])
+                let grade = parseInt(origin.data('grade'))
+                // 顶层
+                if(grade === 0) {
+                    let tmp = []
+                    tmp.push(name)
+                    dragList.push(tmp)
+                } else if(grade === 1) {
+                    let index = origin.data('index')
+                    let item = [].concat(dragList[index] || [])
+                    item.push(name)
+                    dragList[index] = item
+                }
+                Object.assign(this.$data, {
+                    dragList: dragList
+                })
             }
         }
     })
 });
-
